5cdb4b5065c73576698654e318771356b2cdd397
Сергей Ковальский <237264@mail.ru>
2023-01-22 19:59:46 +0300

    first commit

---
+++ 3.2-crud/stocks_products/stocks_products/.env.py
@@ -0,0 +1,9 @@
+SECRET_KEY=5v(5)0@y5hf#^3q*w__*_#()qhoaua+2^4=)uh(hxbz3)w#hic
+DEBUG=True
+ALLOWED_HOSTS=95.163.233.74
+ENGINE=django.db.backends.postgresql
+NAME=netology_stocks_products
+USER=postgres
+PASSWORD=1988
+HOST=localhost
+PORT=5432
--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nw^y+m^wmxza1asgk+)!ua2qx9)g+#v=6%76-9i8i(6eqiw94j'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [i.strip() for i in v.split(',')])
 
 
 # Application definition
@@ -80,12 +81,12 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_stocks_products',
-        'USER': 'postgres',
-        'PASSWORD': 1988,
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': config('ENGINE'),
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD', cast=int),
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),
     }
 }
 
