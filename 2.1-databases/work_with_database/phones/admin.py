b68f7b359246634d4e7860fe65688408b68ef371
Сергей Ковальский <237264@mail.ru>
2023-01-24 11:26:01 +0300

    raname delete

--- 2.1-databases/work_with_database/phones/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Phone
-
-
-@admin.register(Phone)
-class PhoneAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'price', 'release_date', 'lte_exists',]
-    list_filter = ['id', 'name', 'price', 'release_date', 'lte_exists',]
